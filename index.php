17de89a407f63fdf2566aaafec301025655f6b7b
xabeeb <eng.xabeeb2014@gmail.com>
2019-10-12 20:43:01 +0300

    print H & w

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1 >xabeeb</h1>
+        <?php
+               $txt="hello world";
+               $x=71;
+               $y=4.5;
+               echo $txt;
+               echo "<br>";
+               echo $x;
+               echo "<br>";
+               echo $y;
+        ?>
+    </body>
+</html>
